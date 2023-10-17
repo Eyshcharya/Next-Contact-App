b90df34585707c0e13b431a7bc357a62b94c082e
Eyshcharya <eyshcharya@gmail.com>
2023-10-17 17:36:43 +0530

    Implemented the Layout for Login/Register Pages

---
+++ Layout/Layout1.tsx
@@ -0,0 +1,10 @@
+const Layout1 = () => {
+  return (
+    <>
+      <div className='min-h-screen bg-[url(../public/Background.png)]'>
+        <div className='min-w-[1502px] min-h-[1502px] absolute top-[-367px] left-[-716px] bg-[#083F46] rounded-full'></div>
+      </div>
+    </>
+  );
+};
+export default Layout1;
