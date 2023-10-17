60de2a6ed97a624fbaac18460ff69018f2f69403
Eyshcharya <eyshcharya@gmail.com>
2023-10-17 17:42:13 +0530

    Modified the Code

--- pages/login.tsx
+++ pages/login.tsx
@@ -5,10 +5,7 @@ import { PageWithLayout1 } from "./_app";
 const LoginPage: PageWithLayout1 = () => {
   return (
     <>
-      {/* <div className='min-h-screen bg-[url(../public/Background.png)]'>
-        <div className='min-w-[1502px] min-h-[1502px] absolute top-[-367px] left-[-716px] bg-[#083F46] rounded-full'></div>
-      </div> */}
-      <h3 className='bg-slate-50 relative top-[5rem]'>fjhfg</h3>
+      <h3 className='bg-slate-50 relative top-[5rem]'>Hello</h3>
     </>
   );
 };
