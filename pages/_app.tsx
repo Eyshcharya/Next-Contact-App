22ed1f107df8f27a7260bfe86692272a569f6871
Eyshcharya <eyshcharya@gmail.com>
2023-10-18 13:17:00 +0530

    Refactored the code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,6 @@
 import "@/styles/globals.css";
-import type { ReactElement, ReactNode } from "react";
-import type { NextPage } from "next";
 import type { AppProps } from "next/app";
 
-export type PageWithLayout1<P = {}, IP = P> = NextPage<P, IP> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: PageWithLayout1;
-};
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
