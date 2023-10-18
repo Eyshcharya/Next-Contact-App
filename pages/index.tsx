0a3b1352a3d5a0d3caa8b8bc57ff011910b6c4f4
Eyshcharya <eyshcharya@gmail.com>
2023-10-18 18:31:43 +0530

    Changed a className

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const HomePage = () => {
     <>
       <div className='text-white absolute top-[208px] left-[242px]'>
         <section className='h-[180px] w-[950px] mb-5'>
-          <h1 className='font-700 text-[50px] leading-[73px]'>Welcome,</h1>
+          <h1 className='font-bold text-[50px] leading-[73px]'>Welcome,</h1>
           <p className='font-400 text-[35px] leading-[55px]'>
             This is where your contacts will live. Click the button below to add
             a new contact.
