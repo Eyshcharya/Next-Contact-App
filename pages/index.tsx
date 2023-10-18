8ae446ec5df3e819e0b071edb3a1bd807848fb34
Eyshcharya <eyshcharya@gmail.com>
2023-10-18 17:19:21 +0530

    Implemented the Welcome Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,29 @@
+import { useRouter } from "next/router";
+
 const HomePage = () => {
-  return <div>Home</div>;
+  const router = useRouter();
+  const handleAddContact = () => {
+    router.push("/contacts/new");
+  };
+  return (
+    <>
+      <div className='text-white absolute top-[208px] left-[242px]'>
+        <section className='h-[180px] w-[950px] mb-5'>
+          <h1 className='font-700 text-[50px] leading-[73px]'>Welcome,</h1>
+          <p className='font-400 text-[35px] leading-[55px]'>
+            This is where your contacts will live. Click the button below to add
+            a new contact.
+          </p>
+        </section>
+
+        <button
+          className='btn-primary text-[25px] w-[323px] h-[48px] mt-12'
+          onClick={handleAddContact}
+        >
+          add a new contact
+        </button>
+      </div>
+    </>
+  );
 };
 export default HomePage;
