144422cd9b2536fca5d2c7adac766a698d2cae21
Eyshcharya <eyshcharya@gmail.com>
2023-10-19 16:16:44 +0530

    Implemented the Helper functions to GET/POST/PUT/DELETE contacts

---
+++ libs/helper.ts
@@ -0,0 +1,59 @@
+import { Contact } from "../Utils/types";
+
+export const BASE_URL = "http://localhost:3000/api/";
+
+// GET contacts
+export const getContacts = async () => {
+  const response = await fetch(`${BASE_URL}/contacts`);
+  const data = await response.json();
+  return data;
+};
+
+// POST contact
+export const addContact = async (contact: Contact) => {
+  try {
+    const Options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(contact),
+    };
+    const response = await fetch(`${BASE_URL}/contacts`, Options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return console.log("error:", error);
+  }
+};
+
+// PUT contact
+export const updateContact = async (contactId: string, contact: Contact) => {
+  try {
+    const Options = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(contact),
+    };
+
+    const response = await fetch(`${BASE_URL}/contacts?${contactId}`, Options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return console.log("error:", error);
+  }
+};
+
+// DELETE contact
+export const deleteContact = async (contactId: string) => {
+  try {
+    const Options = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    const response = await fetch(`${BASE_URL}/contacts?${contactId}`, Options);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return console.log("error:", error);
+  }
+};
