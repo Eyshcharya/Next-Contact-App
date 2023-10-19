aabbbf112dd0a1107fd316113315a84193dea9c1
Eyshcharya <eyshcharya@gmail.com>
2023-10-19 16:15:01 +0530

    Refactored the code

--- Utils/types.ts
+++ Utils/types.ts
@@ -1,11 +1,4 @@
-export interface ResponseFuncs {
-  GET?: Function;
-  POST?: Function;
-  PUT?: Function;
-  DELETE?: Function;
-}
 export interface Contact {
-  _id?: number;
   name: string;
   email: string;
   phone: string;
