f0151f66973e75ca967ced9d8685fb7fd8e47c6c
Eyshcharya <eyshcharya@gmail.com>
2023-10-20 14:40:29 +0530

    Implemented Custom error page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,12 @@
 const ErrorPage = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className='text-white absolute top-[208px] left-[242px]'>
+        <section className='h-[180px] w-[950px] mb-5'>
+          <h1 className='font-bold text-[50px] leading-[73px]'>{`404 - Page Not Found :(`}</h1>
+        </section>
+      </div>
+    </>
+  );
 };
 export default ErrorPage;
