272f345d2782e343df968084cda8cdf73b99a238
Eyshcharya <eyshcharya@gmail.com>
2023-10-20 13:56:26 +0530

    Implemented update contact feature

--- components/SingleContact.tsx
+++ components/SingleContact.tsx
@@ -16,9 +16,19 @@ const SingleContact = ({
   refetch,
 }: PageProps) => {
   const router = useRouter();
+
+  // component state
   const [modal, setModal] = useState(false);
+  const [editable, setEditable] = useState(false);
   const [deleteSuccess, setDeleteSuccess] = useState(false);
   const [editSuccess, setEditSuccess] = useState(false);
+  const [Gender, setGender] = useState(gender);
+
+  // controlled inputs
+  const [editGender, setEditGender] = useState("");
+  const [editName, setEditName] = useState("");
+  const [editEmail, setEditEmail] = useState("");
+  const [editPhone, setEditPhone] = useState("");
 
   // shallow route
   const shallowRoute = () => {
@@ -31,41 +41,144 @@ const SingleContact = ({
     deleteContact(_id);
     setModal(true);
     setDeleteSuccess(true);
+    refetch(true);
   };
 
-  // edit contact
-  const handleEditBtn = () => {
-    shallowRoute();
-
+  // update contact
+  const handleForm = (e: Event) => {
+    e.preventDefault();
+    const contact: Contact = {
+      _id: _id,
+      name: editName || name,
+      email: editEmail || email,
+      phone: editPhone || phone,
+      gender: editGender || gender,
+    };
+    setEditable(false);
+    updateContact(_id, contact);
     setModal(true);
     setEditSuccess(true);
+    refetch(true);
   };
 
+  // Enable editing
+  const handleEditBtn = () => {
+    setEditable(true);
+    shallowRoute();
+  };
+
+  // switch genders
+  const handleSwitchBtn = (g: string) => {
+    if (g === "male") {
+      setGender("female");
+      setEditGender("female");
+    } else if (g === "female") {
+      setGender("male");
+      setEditGender("male");
+    }
+  };
+
+  // handle modal
   const handleModal = () => {
     setModal(false);
     setDeleteSuccess(false);
     setEditSuccess(false);
-    refetch(true);
   };
+
   return (
     <>
-      <div className='grid-style text-[20px]'>
-        <div className='pl-10 mt-[-10px] pb-8'>
-          {gender === "female" ? <FemaleAvatar /> : <MaleAvatar />}
-        </div>
-        <div>{name} </div>
-        <div> {gender}</div>
-        <div> {email}</div>
-        <div> {phone}</div>
-        <div className='flex flex-row gap-4 mt-[-30px]'>
-          <button onClick={handleEditBtn}>
-            <EditIcon />
-          </button>
-          <button onClick={handleDeleteBtn}>
-            <DeleteIcon />
-          </button>
-        </div>
-      </div>
+      {editable ? (
+        <form
+          action=''
+          className='grid-style text-[20px]'
+          onSubmit={handleForm}
+        >
+          {/* avatar */}
+          <div className='pl-10 mt-[-10px] pb-8'>
+            {Gender === "female" ? <FemaleAvatar /> : <MaleAvatar />}
+          </div>
+
+          {/* name */}
+          <div>
+            <input
+              type='text'
+              className='edit-contact'
+              defaultValue={name}
+              onChange={(e) => setEditName(e.target.value)}
+            />
+          </div>
+
+          {/* gender */}
+          <div>
+            <input
+              type='text'
+              className='absolute left-[365px] w-[155px]  edit-contact'
+              disabled={true}
+            />
+            <div className='text-left pl-2 pt-1 relative'>
+              {Gender}
+              <button
+                className='absolute left-[90px] top-[6px]'
+                onClick={() => {
+                  handleSwitchBtn(Gender);
+                }}
+              >
+                <ArrowIcon />
+              </button>
+            </div>
+          </div>
+
+          {/* email */}
+          <div>
+            <input
+              type='email'
+              defaultValue={email}
+              className='edit-contact'
+              onChange={(e) => setEditEmail(e.target.value)}
+            />
+          </div>
+
+          {/* phone */}
+          <div>
+            <input
+              type='number'
+              className='edit-contact'
+              defaultValue={phone}
+              onChange={(e) => setEditPhone(e.target.value)}
+            />
+          </div>
+
+          {/* save-btn */}
+          <div className=''>
+            <button
+              className='bg-[#083f46] text-white rounded-[2rem] py-1 px-4'
+              type='submit'
+            >
+              save
+            </button>
+          </div>
+        </form>
+      ) : (
+        <>
+          <div className='grid-style text-[20px]'>
+            <div className='pl-10 mt-[-10px] pb-8'>
+              {gender === "female" ? <FemaleAvatar /> : <MaleAvatar />}
+            </div>
+            <div>{name} </div>
+            <div> {gender}</div>
+            <div> {email}</div>
+            <div> {phone}</div>
+            <div className='flex flex-row gap-5 mt-[-45px]'>
+              <button onClick={handleEditBtn}>
+                <EditIcon />
+              </button>
+              <button onClick={handleDeleteBtn}>
+                <DeleteIcon />
+              </button>
+            </div>
+          </div>
+        </>
+      )}
 
       {/* modal */}
       {modal && (
